a3b49d33d0972ab32d77f0b63749615ac44ba630
Wilfredo <szr.wilfredo@gmail.com>
2020-05-03 13:13:58 +0200

    commenting func in macro.py

--- macro.py
+++ macro.py
@@ -34,61 +34,61 @@ print '\n'
 user = 'amersp-wilszr'
 pw = 'AmerMar2020.'
 
-def telnet_vz(ip):
-
-    t = telnetlib.Telnet(ip)
-    t.read_until(b"Username:")
-    t.write(user.encode("ascii") + b"\n")
-
-    if pw:
-        t.read_until(b"Password:")
-        t.write(pw.encode("ascii") + b"\n")
-        print '\n***** TELNET connection successfull to ' + ip + ' ***** \n'
-
-    for c in cmd_list:
-        t.write(c + "\n")
-
-    t.write(b"exit\n")
-
-    text = t.read_all()
-
-    print text
-    print ('\n' + '=' * 120 + '\n')
-
-    f = open(output_text,'a')
-    f.write('\n***** TELNET connection to ' + ip + ' ***** \n')
-    f.write(text) 
-    f.write('\n' + '=' * 120 + '\n')
-    f.close()
-
-def ssh_vz(ip):
-
-    twrssh = paramiko.SSHClient()
-    twrssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    twrssh.connect(ip, port=22, username=user, password=pw)
-    print '\n***** SSH connection successfull to ' + ip + ' ****** \n'
-
-    remote = twrssh.invoke_shell()
-
-    for c in cmd_list:
-        remote.send(c + '\n')
-        time.sleep(1)
-   
-    remote.send('exit\n')
-    time.sleep(1)    
-   
-    buf = remote.recv(65000)
-
-    print buf
-    print ('\n' + '=' * 120 + '\n')
-
-    f = open(output_text,'a')
-    f.write('\n***** SSH connection to ' + ip + ' ***** \n\n' )
-    f.write(buf) 
-    f.write('\n' + '=' * 120 + '\n')
-    f.close()
-    
-    twrssh.close()
+#def telnet_vz(ip):
+#
+#    t = telnetlib.Telnet(ip)
+#    t.read_until(b"Username:")
+#    t.write(user.encode("ascii") + b"\n")
+#
+#    if pw:
+#        t.read_until(b"Password:")
+#        t.write(pw.encode("ascii") + b"\n")
+#        print '\n***** TELNET connection successfull to ' + ip + ' ***** \n'
+#
+#    for c in cmd_list:
+#        t.write(c + "\n")
+#
+#    t.write(b"exit\n")
+#
+#    text = t.read_all()
+#
+#    print text
+#    print ('\n' + '=' * 120 + '\n')
+#
+#    f = open(output_text,'a')
+#    f.write('\n***** TELNET connection to ' + ip + ' ***** \n')
+#    f.write(text) 
+#    f.write('\n' + '=' * 120 + '\n')
+#    f.close()
+#
+#def ssh_vz(ip):
+#
+#    twrssh = paramiko.SSHClient()
+#    twrssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#    twrssh.connect(ip, port=22, username=user, password=pw)
+#    print '\n***** SSH connection successfull to ' + ip + ' ****** \n'
+#
+#    remote = twrssh.invoke_shell()
+#
+#    for c in cmd_list:
+#        remote.send(c + '\n')
+#        time.sleep(1)
+#   
+#    remote.send('exit\n')
+#    time.sleep(1)    
+#   
+#    buf = remote.recv(65000)
+#
+#    print buf
+#    print ('\n' + '=' * 120 + '\n')
+#
+#    f = open(output_text,'a')
+#    f.write('\n***** SSH connection to ' + ip + ' ***** \n\n' )
+#    f.write(buf) 
+#    f.write('\n' + '=' * 120 + '\n')
+#    f.close()
+#    
+#    twrssh.close()
     
 
 def main():
